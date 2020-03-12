88ca823b9372ad9baa652511cf974298315f9dd6
yopijay <judan.pauljohn@gmail.com>
2020-03-12 17:55:36 +0800

    Buttons (Update)
    Some modification on buttons design

--- resources/views/layout/button.blade.php
+++ resources/views/layout/button.blade.php
@@ -3,43 +3,35 @@
     <div class= "card mb-4">
         <div class= "card-header">Button Normal</div>
         <div class= "card-body text-center">
-            <button class= "btn mx-2">Normal</button>
-            <button class= "btn btn-primary mx-2">Primary</button>
-            <button class= "btn btn-secondary mx-2">Secondary</button>
-            <button class= "btn btn-success mx-2">Success</button>
-            <button class= "btn btn-warning mx-2">Warning</button>
-            <button class= "btn btn-danger mx-2">Danger</button>
-            <button class= "btn btn-info mx-2">Info</button>
-            <button class= "btn btn-light mx-2">Light</button>
-            <button class= "btn btn-dark mx-2">Dark</button>
-        </div>
-    </div>
-    <div class= "card mb-4">
-        <div class= "card-header">Button Outline</div>
-        <div class= "card-body text-center">
-            <button class= "btn btn-outline mx-2">Normal</button>
-            <button class= "btn btn-primary-outline mx-2">Primary</button>
-            <button class= "btn btn-secondary-outline mx-2">Secondary</button>
-            <button class= "btn btn-success-outline mx-2">Success</button>
-            <button class= "btn btn-warning-outline mx-2">Warning</button>
-            <button class= "btn btn-danger-outline mx-2">Danger</button>
-            <button class= "btn btn-info-outline mx-2">Info</button>
-            <button class= "btn btn-light-outline mx-2">Light</button>
-            <button class= "btn btn-dark-outline mx-2">Dark</button>
-        </div>
-    </div>
-    <div class= "card mb-4">
-        <div class= "card-header">Button Pills</div>
-        <div class= "card-body text-center">
-            <button class= "btn btn-pills mx-2">Normal</button>
-            <button class= "btn btn-primary-pills mx-2">Primary</button>
-            <button class= "btn btn-secondary-pills mx-2">Secondary</button>
-            <button class= "btn btn-success-pills mx-2">Success</button>
-            <button class= "btn btn-warning-pills mx-2">Warning</button>
-            <button class= "btn btn-danger-pills mx-2">Danger</button>
-            <button class= "btn btn-info-pills mx-2">Info</button>
-            <button class= "btn btn-light-pills mx-2">Light</button>
-            <button class= "btn btn-dark-pills mx-2">Dark</button>
+            <div class= "row mb-1">
+                <div class= "col-md-3">
+                    <button class= "btn mx-2 w-100 mb-2">.btn</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-primary mx-2 w-100 mb-2">.btn-primary</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-secondary mx-2 w-100 mb-2">.btn-secondary</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-success mx-2 w-100 mb-2">.btn-success</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-warning mx-2 w-100 mb-2">.btn-warning</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-danger mx-2 w-100 mb-2">.btn-danger</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-info mx-2 w-100 mb-2">.btn-info</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-light mx-2 w-100 mb-2">.btn-light</button>
+                </div>
+                <div class= "col-md-3">
+                    <button class= "btn btn-dark mx-2 w-100 mb-2">.btn-dark</button>
+                </div>
+            </div>
         </div>
     </div>
     <div class= "card mb-4">
