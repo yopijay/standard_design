92cfec9c436641f174961e9ce90e3ec6f3614e43
yopijay <judan.pauljohn@gmail.com>
2020-03-30 03:49:47 +0800

    Typography and buttons
    Create the ui for the typography page and modify design of buttons when focused

--- resources/views/components/themes/typography.blade.php
+++ resources/views/components/themes/typography.blade.php
@@ -2,4 +2,73 @@
     <h4 class= "mb-5">TYPOGRAPHY</h4>
     <p class= "mb-1">Documentation and examples for Bootstrap typography, including global settings, headings, body text, lists, and more.</p>
     <hr>
+    <div class= "row mb-4">
+        <div class= "col-md-12 my-4">
+            <div class= "row">
+                <div class= "col-md-3">
+                    <p class= "h5 text-info">Headings</p>
+                </div>
+                <div class= "col-md-9">
+                    <p class= "h5 text-info">Examples</p>
+                </div>
+            </div>
+        </div>
+        <div class= "col-md-12 my-2">
+            <div class= "row">
+                <div class= "col-md-3">
+                    <p class= "text-danger">< h1 >< / h1></p>
+                </div>
+                <div class= "col-md-9">
+                    <h1>Bootstrap heading</h1>
+                </div>
+            </div>
+        </div>
+        <div class= "col-md-12 my-2">
+            <div class= "row">
+                <div class= "col-md-3">
+                    <p class= "text-danger">< h2 >< / h2></p>
+                </div>
+                <div class= "col-md-9">
+                    <h2>Bootstrap heading</h2>
+                </div>
+            </div>
+        </div>
+        <div class= "col-md-12 my-2">
+            <div class= "row">
+                <div class= "col-md-3">
+                    <p class= "text-danger">< h3 >< / h3></p>
+                </div>
+                <div class= "col-md-9">
+                    <h3>Bootstrap heading</h3>
+                </div>
+            </div>
+        </div>
+        <div class= "col-md-12 my-2">
+            <div class= "row">
+                <div class= "col-md-3">
+                    <p class= "text-danger">< h4 >< / h4></p>
+                </div>
+                <div class= "col-md-9">
+                    <h4>Bootstrap heading</h4>
+                </div>
+            </div>
+        </div>
+        <div class= "col-md-12 my-2">
+            <div class= "row">
+                <div class= "col-md-3">
+                    <p class= "text-danger">< h5 >< / h5></p>
+                </div>
+                <div class= "col-md-9">
+                    <h5>Bootstrap heading</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class= "mb-1"><span class= "text-danger">.h1</span> through <span class= "text-danger">.h5</span> classes are also available, for when you want to match the font styling of a heading but cannot use the associated HTML element.</p>
+    <hr>
+    <p class= "h1 my-3"><span class= "text-danger">.h1</span> Bootstrap heading.</p>
+    <p class= "h2 my-3"><span class= "text-danger">.h2</span> Bootstrap heading.</p>
+    <p class= "h3 my-3"><span class= "text-danger">.h3</span> Bootstrap heading.</p>
+    <p class= "h4 my-3"><span class= "text-danger">.h4</span> Bootstrap heading.</p>
+    <p class= "h5 my-3"><span class= "text-danger">.h5</span> Bootstrap heading.</p>
 </div>
