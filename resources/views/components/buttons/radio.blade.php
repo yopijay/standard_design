0e1322f0990f87fe398990eaeeaec65a9e6aecfd
username <email>
2020-05-08 14:25:12 +0800

    Update radio.blade.php

--- resources/views/components/buttons/radio.blade.php
+++ resources/views/components/buttons/radio.blade.php
@@ -4,20 +4,21 @@
     <p class= "text-info mb-1 h5">NORMAL</p>
     <p class= "mb-4">Add <span class= "text-danger">.radio-* {primary , secondary, success, warning, danger, info, light, dark}</span> for your desired checkbox color.</p>
     <div class= "row mt-3 mb-5">
-        <div class="col-md-5">
+        <div class="col-md-6 py-3 agm-bg-light rounded">
+            <p class= "text-info mb-3 h5">Preview:</p>
             <div class= "form-radio">
                 <input type= "radio" id= "radio-default">
                 <span></span>
                 <label for= "radio-default">Radio default</label>
             </div>
         </div>
-        <div class= "col-md-7">
+        <div class= "col-md-6 py-3">
             <p class= "h5 text-info mb-3">Example Code:</p>
-            <p class= "mb-1">< div class= "<span class= "text-danger">form-radio</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< input type= "<span class= "text-danger">radio</span>" id= "<span class= "text-danger">radio-default</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< span>< /span ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< label for= "<span class= "text-danger">radio-default</span>" >Normal checkbox< /label ></p>
-            <p class= "mb-1">< /div ></p>
+            <p class= "mb-1">< <span class= "text-danger">div</span> class= "<span class= "text-danger">form-radio</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">input</span> type= "<span class= "text-danger">radio</span>" id= "<span class= "text-danger">radio-default</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">span</span> >< <span class= "text-danger">/span</span> ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">label</span> for= "<span class= "text-danger">radio-default</span>" >Normal checkbox< <span class= "text-danger">/labe</span>l ></p>
+            <p class= "mb-1">< <span class= "text-danger">/div</span> ></p>
             <p id= "note" class= "text-warning"><i class= "fas fa-info-circle"></i>Just remove the spaces when you copy it!</p>
         </div>
     </div>
