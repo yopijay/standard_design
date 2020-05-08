f41b8f3bb28e76aec737df7f5d1860ffa0532bef
username <email>
2020-05-08 14:42:24 +0800

    Update toggle.blade.php

--- resources/views/components/buttons/toggle.blade.php
+++ resources/views/components/buttons/toggle.blade.php
@@ -4,90 +4,95 @@
     <p class= "text-info mb-1 h5">NORMAL TOGGLE</p>
     <p class= "mb-4">Add <span class= "text-danger">.toggle-* {primary , secondary, success, warning, danger, info, light, dark}</span> for your desired checkbox color.</p>
     <div class= "row mt-3 mb-5">
-        <div class= "col-md-5">
+        <div class= "col-md-6 py-3 agm-bg-light rounded">
+            <p class= "text-info mb-3 h5">Preview:</p>
             <div class= "form-toggle toggle-default">
                 <input type= "checkbox" id= "toggle">
                 <label for= "toggle"></label>
             </div>
         </div>
-        <div class= "col-md-7">
+        <div class= "col-md-6 py-3">
             <p class= "h5 text-info mb-3">Example Code:</p>
-            <p class= "mb-1">< div class= "<span class= "text-danger">form-toggle toggle-default</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< input type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< label for= "<span class= "text-danger">toggle</span>" >< /label ></p>
-            <p class= "mb-1">< /div ></p>
+            <p class= "mb-1">< <span class= "text-danger">div</span> class= "<span class= "text-danger">form-toggle toggle-default</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">input</span> type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">label</span> for= "<span class= "text-danger">toggle</span>" >< <span class= "text-danger">/label</span> ></p>
+            <p class= "mb-1">< <span class= "text-danger">/div</span> ></p>
             <p id= "note" class= "text-warning"><i class= "fas fa-info-circle"></i>Just remove the spaces when you copy it!</p>
         </div>
     </div>
     <p class= "text-info mb-1 h5">IOS TOGGLE</p>
     <p class= "mb-4">Add <span class= "text-danger">.toggle-* {primary , secondary, success, warning, danger, info, light, dark}</span> for your desired checkbox color.</p>
     <div class= "row mt-3 mb-5">
-        <div class= "col-md-5">
+        <div class= "col-md-6 py-3 agm-bg-light rounded">
+            <p class= "text-info mb-3 h5">Preview:</p>
             <div class= "form-toggle toggle-ios">
                 <input type= "checkbox" id= "toggle-ios">
                 <label for= "toggle-ios"></label>
             </div>
         </div>
-        <div class= "col-md-7">
+        <div class= "col-md-6 py-3">
             <p class= "h5 text-info mb-3">Example Code:</p>
-            <p class= "mb-1">< div class= "<span class= "text-danger">form-toggle toggle-ios</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< input type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-ios</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< label for= "<span class= "text-danger">toggle-ios</span>" >< /label ></p>
-            <p class= "mb-1">< /div ></p>
+            <p class= "mb-1">< <span class= "text-danger">div</span> class= "<span class= "text-danger">form-toggle toggle-ios</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">input</span> type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-ios</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">label</span> for= "<span class= "text-danger">toggle-ios</span>" >< <span class= "text-danger">/label</span> ></p>
+            <p class= "mb-1">< <span class= "text-danger">/div</span> ></p>
             <p id= "note" class= "text-warning"><i class= "fas fa-info-circle"></i>Just remove the spaces when you copy it!</p>
         </div>
     </div>
     <p class= "text-info mb-1 h5">SKEWED TOGGLE</p>
     <p class= "mb-4">Add <span class= "text-danger">.toggle-* {primary , secondary, success, warning, danger, info, light, dark}</span> for your desired checkbox color.</p>
     <div class= "row mt-3 mb-5">
-        <div class= "col-md-5">
+        <div class= "col-md-6 py-3 agm-bg-light rounded">
+            <p class= "text-info mb-3 h5">Preview:</p>
             <div class= "form-toggle toggle-skewed">
                 <input type= "checkbox" id= "toggle-skewed">
                 <label for= "toggle-skewed" data-tg-off= "OFF" data-tg-on= "ON"></label>
             </div>
         </div>
-        <div class= "col-md-7">
+        <div class= "col-md-6 py-3">
             <p class= "h5 text-info mb-3">Example Code:</p>
-            <p class= "mb-1">< div class= "<span class= "text-danger">form-toggle toggle-skewed</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< input type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-skewed</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< label for= "<span class= "text-danger">toggle-skewed</span>" data-tg-off= "<span class= "text-danger">OFF</span>" data-tg-on= "<span class= "text-danger">ON</span>" >< /label ></p>
-            <p class= "mb-1">< /div ></p>
+            <p class= "mb-1">< <span class= "text-danger">div</span> class= "<span class= "text-danger">form-toggle toggle-skewed</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">input</span> type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-skewed</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">label</span> for= "<span class= "text-danger">toggle-skewed</span>" data-tg-off= "<span class= "text-danger">OFF</span>" data-tg-on= "<span class= "text-danger">ON</span>" >< <span class= "text-danger">/label</span> ></p>
+            <p class= "mb-1">< <span class= "text-danger">/div</span> ></p>
             <p id= "note" class= "text-warning"><i class= "fas fa-info-circle"></i>Just remove the spaces when you copy it!</p>
         </div>
     </div>
     <p class= "text-info mb-1 h5">FLAT TOGGLE</p>
     <p class= "mb-4">Add <span class= "text-danger">.toggle-* {primary , secondary, success, warning, danger, info, light, dark}</span> for your desired checkbox color.</p>
     <div class= "row mt-3 mb-5">
-        <div class= "col-md-5">
+        <div class= "col-md-6 py-3 agm-bg-light rounded">
+            <p class= "text-info mb-3 h5">Preview:</p>
             <div class= "form-toggle toggle-flat">
                 <input type= "checkbox" id= "toggle-flat">
                 <label for= "toggle-flat" data-tg-off= "OFF" data-tg-on= "ON"></label>
             </div>
         </div>
-        <div class= "col-md-7">
+        <div class= "col-md-6 py-3">
             <p class= "h5 text-info mb-3">Example Code:</p>
-            <p class= "mb-1">< div class= "<span class= "text-danger">form-toggle toggle-flat</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< input type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-flat</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< label for= "<span class= "text-danger">toggle-flat</span>" data-tg-off= "<span class= "text-danger">OFF</span>" data-tg-on= "<span class= "text-danger">ON</span>" >< /label ></p>
-            <p class= "mb-1">< /div ></p>
+            <p class= "mb-1">< <span class= "text-danger">div</span> class= "<span class= "text-danger">form-toggle toggle-flat</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">input</span> type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-flat</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">label</span> for= "<span class= "text-danger">toggle-flat</span>" data-tg-off= "<span class= "text-danger">OFF</span>" data-tg-on= "<span class= "text-danger">ON</span>" >< <span class= "text-danger">/label</span> ></p>
+            <p class= "mb-1">< <span class= "text-danger">/div</span> ></p>
             <p id= "note" class= "text-warning"><i class= "fas fa-info-circle"></i>Just remove the spaces when you copy it!</p>
         </div>
     </div>
     <p class= "text-info mb-1 h5">FLIP TOGGLE</p>
     <p class= "mb-4">Add <span class= "text-danger">.toggle-* {primary , secondary, success, warning, danger, info, light, dark}</span> for your desired checkbox color.</p>
     <div class= "row mt-3 mb-5">
-        <div class= "col-md-5">
+        <div class= "col-md-6 py-3 agm-bg-light rounded">
+            <p class= "text-info mb-3 h5">Preview:</p>
             <div class= "form-toggle toggle-flip">
                 <input type= "checkbox" id= "toggle-flip">
                 <label for= "toggle-flip" data-tg-off= "OFF" data-tg-on= "ON"></label>
             </div>
         </div>
-        <div class= "col-md-7">
+        <div class= "col-md-6 py-3">
             <p class= "h5 text-info mb-3">Example Code:</p>
-            <p class= "mb-1">< div class= "<span class= "text-danger">form-toggle toggle-flip</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< input type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-flip</span>" ></p>
-            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< label for= "<span class= "text-danger">toggle-flip</span>" data-tg-off= "<span class= "text-danger">OFF</span>" data-tg-on= "<span class= "text-danger">ON</span>" >< /label ></p>
-            <p class= "mb-1">< /div ></p>
+            <p class= "mb-1">< <span class= "text-danger">div</span> class= "<span class= "text-danger">form-toggle toggle-flip</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">input</span> type= "<span class= "text-danger">checkbox</span>" id= "<span class= "text-danger">toggle-flip</span>" ></p>
+            <p class= "mb-1">&nbsp;&nbsp;&nbsp;< <span class= "text-danger">label</span> for= "<span class= "text-danger">toggle-flip</span>" data-tg-off= "<span class= "text-danger">OFF</span>" data-tg-on= "<span class= "text-danger">ON</span>" >< <span class= "text-danger">/label</span> ></p>
+            <p class= "mb-1">< <span class= "text-danger">/div</span> ></p>
             <p id= "note" class= "text-warning"><i class= "fas fa-info-circle"></i>Just remove the spaces when you copy it!</p>
         </div>
     </div>
