a2a8fa3262bf6123aeca127629de3b2eefd74fce
yopijay <judan.pauljohn@gmail.com>
2020-05-22 13:48:56 +0800

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -49672,9 +49672,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\User\Desktop\PJ\standard_design\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\User\Desktop\PJ\standard_design\resources\sass\agm-standard.scss */"./resources/sass/agm-standard.scss");
-module.exports = __webpack_require__(/*! C:\Users\User\Desktop\PJ\standard_design\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\GIT\standard_design\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\xampp\htdocs\GIT\standard_design\resources\sass\agm-standard.scss */"./resources/sass/agm-standard.scss");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\GIT\standard_design\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
